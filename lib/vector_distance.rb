20ab354470d03acd7cab2553185506dca8d51219
Samantha Yeargin <slyeargin@gmail.com>
2014-07-22 11:25:17 -0500

    Refactor after class example

--- lib/vector_distance.rb
+++ lib/vector_distance.rb
@@ -3,9 +3,7 @@ require 'matrix'
 class VectorSubclass < Vector
 
   def distance(to_vector)
-    from_vector = covector(self)
-    to_vector = covector(to_covector)
-    Math.sqrt((from_vector[0]-to_vector[0])**2 + ((from_vector[1]-to_vector[1])**2))
+    Math.sqrt((self[0]-to_vector[0])**2 + ((self[1]-to_vector[1])**2))
   end
 
 end
