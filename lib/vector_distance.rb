527584bd3900f6a24cbec83f3ee4c4979a88e1fd
Samantha Yeargin <slyeargin@gmail.com>
2014-07-22 11:27:53 -0500

    Added notes.

--- lib/vector_distance.rb
+++ lib/vector_distance.rb
@@ -1,7 +1,8 @@
 require 'matrix'
 
-class VectorSubclass < Vector
+class MyVector < Vector
 
+  # called by MyVector[x,y].distance(Vector[2,2])
   def distance(to_vector)
     Math.sqrt((self[0]-to_vector[0])**2 + ((self[1]-to_vector[1])**2))
   end
