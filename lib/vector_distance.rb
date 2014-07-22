72254fc920a23063bcd92e28699a4b07f786e384
Samantha Yeargin <slyeargin@gmail.com>
2014-07-22 15:04:33 -0500

    Class, instance methods for calculating the angle between two vectors in radians.

--- lib/vector_distance.rb
+++ lib/vector_distance.rb
@@ -25,18 +25,26 @@ class MyVector < Vector
     distance_stash
   end
 
+  def calculate_angle(other_vector)
+    theta = (self.inner_product other_vector)/((Math.sqrt(self[1]**2 + other_vector[1]**2)) + (Math.sqrt(self[0]**2 + other_vector[0]**2)))
+    radians = Math.acos(theta)
+  end
+
+  def self.calculate_angle(v1, v2)
+    theta = (v1.inner_product v2)/((Math.sqrt(v1[1]**2 + v2[1]**2)) + (Math.sqrt(v1[0]**2 + v2[0]**2)))
+    radians = Math.acos(theta)
+  end
+
   def +(a)
-    vector = self.covector.to_a
+    vector = self.to_a
     for v in (0..vector.length-1)
       vector[v] += a
     end
-    Vector[vector.flatten!]
+    MyVector.elements(vector)
   end
 
   def <<(a)
-    vector = self.covector.to_a
-    vector << a
-    Vector[vector.flatten!]
+    MyVector.elements(self.to_a << a)
   end
 
 end
--- test/vector_test.rb
+++ test/vector_test.rb
@@ -10,12 +10,27 @@ class MyVectorTest < MiniTest::Unit::TestCase
     @v4 = MyVector[1,2,3]
   end
 
+  def test_distance
+    distance = @v.distance(@v3)
+    assert_equal(1.0, distance)
+  end
+
   def test_vector_plus_integer
     assert_equal(@v, @v2+1)
   end
 
   def test_vector_shovel_integer
     assert_equal(@v4, @v3 << 3)
+  end
+
+  def test_calculate_angle_in_rad_class
+    angle = MyVector.calculate_angle(@v, @v3)
+    assert_equal(0.6061378223872939, angle)
+  end
+
+  def test_calculate_angle_in_rad_instance
+    angle = @v.calculate_angle(@v3)
+    assert_equal(0.6061378223872939, angle)
   end
 
 end
